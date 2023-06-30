a847d7bcc4521d4dad8ac3724a6e6fa898ab408d
SHIBANGEE <133844978+SHIBANGEE@users.noreply.github.com>
2023-06-30 08:54:03 +0530

    JS-1

---
+++ MODULE-2/ifprogram.js
@@ -0,0 +1,17 @@
+a= 90;
+if (a>80)
+{
+    console.log("A+ GRADE");
+}
+else if(a>50)
+{
+    console.log("A GRADE");
+}
+else if(a>60)
+{
+    console.log("B GRADE");
+}
+else
+{
+    console.log("FAIL");
+}
